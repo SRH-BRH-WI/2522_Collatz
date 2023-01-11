754e3c1cf77ab45cbad57d5b92183d47d6bc4590
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-01-11 15:08:09 +0100

    Bestimme Zahl mit größten Collatzwert

--- src/Collatz.java
+++ src/Collatz.java
@@ -1,9 +1,21 @@
 public class Collatz {
     public static void main(String[] args) {
-        int n = 17;
 
-        // TODO
-
-        System.out.println(n);
+        int maxWert = 0;
+        int maxStart = 0;
+        for (int start = 1; start < 100; start++) {
+            int n = start;
+            int zähler = 0;
+            while (n != 1) {
+                zähler++;
+                if (n % 2 == 1) n = n * 3 + 1;
+                else n = n / 2;
+            }
+            if (zähler > maxWert) {
+                maxWert = zähler;
+                maxStart = start;
+            }
+        }
+        System.out.println(maxStart + " hat den größten Collatzwert " + maxWert);
     }
 }
